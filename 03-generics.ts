3ff7b41db6c4b8ebcb9e4d76890e1628f8239255
Victor Widell <victor.widell@divid.se>
2016-09-01 08:21:00 +0200

    Added example.

--- 03-generics.ts
+++ 03-generics.ts
@@ -43,3 +43,26 @@ const transform3dMatrix = new Array2d<number>(4, 4);
 transform3dMatrix.set(1, 0, 1.2345);
 transform3dMatrix.set(1, 0, 'leet');
 
+
+// The type must be specified explicitly, or it won't work.
+const untyped = new Array2d(4, 4);
+untyped.set(1, 0, 1.2345);
+untyped.set(1, 0, 'leet');
+
+
+
+
+// But generics can be implicitly typed too.
+function map<T1, T2> (a: Array<T1>, f:(x:T1)=>T2): Array<T2> {
+
+	return a.map(f);
+}
+
+// Look at the types of i and x!
+const x = map([1, 2, 3], (i) => 'number: '+i);
+
+// Mixed types are handeled properly too.
+const x2 = map([1, 2, 'three'], (i) => 'number: '+i);
+
+// Invalid function arguments won't compile.
+const x3 = map([1, 2, 'three'], (i) => i*i);
