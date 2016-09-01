c9f7c054400d40c98981a10a9d84d5e1697ba63f
Victor Widell <victor.widell@divid.se>
2016-09-01 09:30:33 +0200

    Added fixed arrays slide.

---
+++ 07-fixed-size-arrays.ts
@@ -0,0 +1,62 @@
+
+// The ES6 datastructure Map has a signature like this:
+function Map<T1, T2> (a: Array<[T1, T2]>): void {}
+
+// It takes an array of 2-element arrays.
+const colors = [
+	['green', '#00ff00'],
+	['orange', '#ff8800'],
+	['lightBlue', '#8899ff']
+];
+
+//...eh?
+const colorMap = new Map(colors);
+
+
+// Typescript sees `colors` as an array of arrays, not fixed-size arrays.
+
+// Cast...
+const colorMapFixed1 = new Map(<Array<[string, string]>> colors);
+const colorMapShouldNotCompile = new Map(<Array<[string, number]>> colors);
+
+// ...or be explicit from the beginnig.
+const colorsFixed = [
+	<[string, string]> ['green', '#00ff00'],
+	<[string, string]> ['orange', '#ff8800'],
+	<[string, string]> ['lightBlue', '#8899ff']
+];
+const colorMapFixed2 = new Map(colorsFixed);
+
+
+
+
+
+
+
+
+
+const numbers = [
+	['pi', 3.1416],
+	['e', 2.7183],
+	['sqrt2', 1.1414]
+];
+
+// Good so far.
+const numberMapFixed1 = new Map(<Array<[string, number]>> numbers);
+
+// FFFFFFFFFFFFFUUUUUUUUUUUUUUUUUUU
+const numberMapShouldNotCompile = new Map(<Array<[string, string]>> numbers);
+
+// This better not be breakable.
+const numbersFixed = [
+	<[string, number]> ['pi', 3.1416],
+	<[string, number]> ['e', 2.7183],
+	<[string, number]> ['sqrt2', 1.1414]
+];
+
+// Phew!
+const numbersFixedBroken = [
+	<[number, string]> ['pi', 3.1416],
+	<[number, string]> ['e', 2.7183],
+	<[number, string]> ['sqrt2', 1.1414]
+];
